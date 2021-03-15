251cd8c689738c898aaa0958b85d3d59b6416dea
adamkcook94 <adamkcook94@hotmail.com>
2021-03-15 23:15:26 +0000

    Added registration functionality

--- app.py
+++ app.py
@@ -23,9 +23,30 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/admin_login")
-def admin_login():
-    return render_template("admin_login.html")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
 
 
 @app.route("/pending_inv")
@@ -38,3 +59,20 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+
+# if request.method == "POST":
+#         existing_user = mongo.db.users.find_one(
+#             {"username": request.form.get("username").lower()
+#              })
+#         if existing_user:
+#             if check_password_hash(
+#                     existing_user["password"], request.form.get("password")):
+#                 session["admin"] = request.form.get("username").lower()
+#             flash("Welcome {}".format(request.form.get("username")))
+#         else:
+#             flash("Incorrect Username or Password")
+#             return redirect(url_for("admin_login"))
+#     else:
+#         flash("Incorrect Username or Password")
+#         return redirect(url_for("admin_login"))
