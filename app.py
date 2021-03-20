72e6901d0a3d2b480db5cab000df21c927c3d10c
adamkcook94 <adamkcook94@hotmail.com>
2021-03-20 08:17:43 +0000

    Completed submission form functionality

--- app.py
+++ app.py
@@ -87,8 +87,25 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/submit_investigation")
+@app.route("/submit_investigation", methods=["GET", "POST"])
 def submit_investigation():
+    if request.method == "POST":
+        investigation = {
+            "crime_name": request.form.get("crime_name"),
+            "family_name": request.form.get("family_name").upper(),
+            "forename": request.form.get("forename"),
+            "gender": request.form.get("gender"),
+            "last_seen": request.form.get("last_seen"),
+            "date_of_birth": request.form.get("date_of_birth"),
+            "nationality": request.form.get("nationality"),
+            "ethnicity": request.form.get("ethnicity"),
+            "phone_number": request.form.get("phone_number"),
+            "email": request.form.get("email"),
+            "additional_info": request.form.get("additional_info"),
+        }
+        mongo.db.pending.insert_one(investigation)
+        flash("Investigation submitted.")
+        return redirect(url_for('home'))
     crime = mongo.db.crime.find().sort("crime_name", 1)
     return render_template("submit_investigation.html", crime=crime)
 
